c5965789ca663c7fc78b0b613173fe6c1faf83f3
lohxinzhi <lxzraizer@gmail.com>
2024-02-25 19:39:50 +0800

    add scanf input and print table

--- bmi.c
+++ bmi.c
@@ -30,7 +30,7 @@ char* returnBMIStatus(float bmi, int asian){ // function to return the status as
     char* bmi_status;
     
                                                                            
-    bmi_status = (char*)malloc(10*sizeof(char)); //allocate memory to store string
+    bmi_status = (char*)malloc(12*sizeof(char)); //allocate memory to store string
     if (bmi < bmi_checkpoints[asian][0]) strcpy(bmi_status, "Low");
     else if (bmi < bmi_checkpoints[asian][1]) strcpy(bmi_status, "Normal");
     else if (bmi <= bmi_checkpoints[asian][2]) strcpy(bmi_status, "Overweight");
@@ -39,10 +39,31 @@ char* returnBMIStatus(float bmi, int asian){ // function to return the status as
     return bmi_status;
 }
 
+void printStatusTable(float mass, float height, float bmi, int asian, char* state, char* bmi_status){ // function to print the status as table
+    printf("\n");
+    printf("|----------------|------------------------\n");
+    printf("|Mass:           |%.2f\n", mass);
+    printf("|----------------|------------------------\n");
+    printf("|Height:         |%.2f\n", height);
+    printf("|----------------|------------------------\n");
+    printf("|BMI:            |%.2f\n", bmi);
+    printf("|----------------|------------------------\n");
+    printf("|Asian/Non-Asian:|");
+    if (asian) {
+        printf("Asian\n");
+    }
+    else {
+        printf("Non-Asian\n");
+        }
+    printf("|----------------|------------------------\n");
+    printf("|BMI State:      |%s\n", state);
+    printf("|----------------|------------------------\n");
+    printf("|Remarks:        |%s\n", bmi_status);
+    printf("|----------------|------------------------\n");
+
+}
+
 void parse_arg(int argc, char const *argv[] ,float *mass, float *height, int *asian){
-    // *mass = atof(argv[1]);
-    // *height = atof(argv[2]);
-    // *asian = atoi(argv[3]);
     int i;
     for (i=1; i< argc; i++){
         if (strcmp(argv[i], "-mass")==0) *mass = atof(argv[++i]);
@@ -59,7 +80,8 @@ void parse_arg(int argc, char const *argv[] ,float *mass, float *height, int *as
                     *asian = 0;
                     break;
                 default:
-                    printf("Invalid input argument for Asian");
+                    *asian = 3; //error
+                    printf("Invalid input argument for Asian. Key in manually later\n\n");
                     break;
             }
         }
@@ -69,28 +91,67 @@ void parse_arg(int argc, char const *argv[] ,float *mass, float *height, int *as
 
 
 int main(int argc, char const *argv[]){
-    float mass = 90.7;
-    float height = 1.79;
-    int asian = 1;
+    float mass = 0.0;
+    float height = 0.0;
+    int asian = 3;
     float bmi;
     float cut;
+    char bmi_status[100];
+    char* state;
+    char temp_asian = 'n';
 
     parse_arg(argc, argv, &mass, &height, &asian);
+    // input error from command line checking
+    if (mass<20 || mass > 200){
+        printf("Enter your mass in kg: ");
+        scanf("%f", &mass);
+    }
+    if (height <0.5 || height > 2.8){
+        printf("Enter your height in m: ");
+        scanf("%f", &height);
+    }
+    if (asian == 3){
+        printf("Are you Asian? (y/n): ");
+        scanf("%s", &temp_asian);
+        switch (temp_asian){
+            case 'y':
+            case 'Y':
+                asian = 1;
+                break;
+            case 'n':
+            case 'N':
+                asian = 0;
+                break;
+            default:
+                asian = 3; //error
+                printf("Invalid input argument for Asian. Key in manually later\n\n");
+                break;
+        }
+    }
+
+
+
     bmi = computeBMI(mass, height);
-    printf("Mass(kg): %.2f\n",mass);
-    printf("Height(m): %.2f\n",height);
-    printf("BMI: %.2f\n",bmi);
+    // printf("Mass(kg): %.2f\n",mass);
+    // printf("Height(m): %.2f\n",height);
+    // printf("BMI: %.2f\n",bmi);
 
-    if (asian) printf("Race: %s\n", "Asian");
-    else printf("Race: %s\n", "Non-Asian");
+    // if (asian) printf("Race: %s\n", "Asian");
+    // else printf("Race: %s\n", "Non-Asian");
 
-    char* state  = returnBMIStatus(bmi, asian);
-    printf("BMI State: %s\n", state);
+    state  = returnBMIStatus(bmi, asian);
+    // printf("BMI State: %s\n", state);
 
-    printf("Remarks: ");
+
+    
+    // printf("Remarks: ");
     cut = (bmi-bmi_checkpoints[asian][1]) * pow(height, 2); // mass to cut to become normal
-    if (cut > 0) printf("You are fat! Please reduce your weight by %.2fkg to remain in the Normal range!\n", cut);
-    else printf("You are in the healthy weight range. Maintain your weight you don't want to be a fat ass\n\n");
+    if (cut > 0)  sprintf(bmi_status, "You are fat! Please reduce your weight by %.2fkg to remain in the Normal range!\n", cut);  
+    else sprintf(bmi_status, "You are in the healthy weight range. Maintain your weight you don't want to be a fat ass\n\n");
+
+    printf(bmi_status);
+    //! print the status as table
+    printStatusTable(mass, height, bmi, asian, state, bmi_status);
 
     free((void*) state);
 
